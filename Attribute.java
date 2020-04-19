208f8cc1b79b6c7c806654f11a3c3d09aa407b22
Marco Fusillo <marcofusillo99@outlook.com>
2020-04-19 16:22:34 +0200

    Aggiunta classe Astratta Attribute

---
+++ Attribute.java
@@ -0,0 +1,37 @@
+	/**
+	 * La classe modella un generico attributo discreto o continuo. 
+	 * 
+	 */
+public abstract class Attribute {
+	
+	String name;
+	int index;	
+	
+	/**
+	 *  E' il costruttore di classe. Inizializza i valori dei membri  name,  index
+	 * @param name
+	 * @param index
+	 */
+	public Attribute(String name,int index) {
+
+		this.index=index;
+		this.name=name;		
+		
+	}
+	/**
+	 * Restituisce il valore nel membro name;
+	 * @return name
+	 */
+	String getName() {
+		return name;
+	}
+	
+	/**
+	 * Restituisce il valore nel membro index; 
+	 * @return index
+	 */
+	int getIndex() {
+		return index;
+	}
+	
+}
