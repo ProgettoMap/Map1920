14bc9d7a0914447f24171f51cb7f5d1b66cb5352
Marco Fusillo <marcofusillo99@outlook.com>
2020-04-19 17:13:57 +0200

    Creazione Classe ContinuosAttribute #2

---
+++ ContinuousAttribute.java
@@ -0,0 +1,18 @@
+/**
+ * Estende la classe Attribute e rappresenta un attributo continuo
+ *
+ */
+public class ContinuousAttribute extends Attribute {
+
+	/**
+	 * Comportamento :Invoca il costruttore della super-classe Input: valori per
+	 * nome simbolico dell'attributo, identificativo numerico dell'attributo
+	 * 
+	 * @param name
+	 * @param index
+	 */
+	public ContinuousAttribute(String name, int index) {
+		super(name, index);
+	}
+
+}
